59f0952b076b208033b37191ab4cc84de4816d8f
Chintan Parikh <parikhcv@gmail.com>
2013-02-04 15:49:29 -0500

    Switched to Feedzirra

--- brotip.rb
+++ brotip.rb
@@ -1,1 +1,3 @@
-require 'httparty'
+require 'feedzirra'
+ATOM_FEED_URL = 'http://www.brotips.com/recent.atom'
+puts Feedzirra::Feed.fetch_and_parse(ATOM_FEED_URL).entries.first.content.gsub('<br>', '')
